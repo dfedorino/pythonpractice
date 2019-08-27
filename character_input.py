6ee47e614a174a1b4ae1beafcd170c4831051061
dfedorino <dfedorino@icloud.com>
2019-08-27 10:02:56 +0800

    Update character_input.py

--- character_input.py
+++ character_input.py
@@ -10,7 +10,8 @@
 # Print out that many copies of the previous message on separate lines. (Hint:
 # the string "\n is the same as pressing the ENTER button)
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# 2019-07-31 - my first solution
 print("""It's time to know the year when you will turn 100 years old. Here are
 several questions for me to know about you:""")
 name = input("Your name:\n")
@@ -24,24 +25,29 @@ print("Now let's play a bit. Give me a number:")
 times = input(">")
 print("Surprise!")
 print(final_message * int(times))
-
+#---------------------------------------------------------------------------------------------------------------------------------------
 # Another solution:
 # import datetime
-
 # now = datetime.datetime.now()
 
 # name = input("Hey, what's your name?\n")
 # age = int(input("Okay {}, how old are you?\n".format(name.title())))
-
 # yrs = (now.year +100) - age
-
 # msg = "Okay {}, so if you're {} now then you should turn 100 in the year {}".format(name.title(),age,yrs)
-
 # print(msg)
-
 # n = int(input("Okay {}, give me a number\n".format(name)))
 
 # print(msg * n)
 # print("Or does this look nicer?")
 # msg += "\n"
 # print(msg * n)
+#---------------------------------------------------------------------------------------------------------------------------------------
+# 2019-08-27 - my refactored solution
+# import datetime
+# now = datetime.datetime.now()
+
+# m = "Okay, {}, you will turn 100 years old in {}\n".format(input("""Hi, user!
+# I'll tell you the year you'll turn 100 y.o. Your name:\n"""),
+# (now.year + 100) - int(input("Your age:\n")))
+# print(m)
+# print(m * int(input("Now let's play a bit. Give me a number:\n>")))
